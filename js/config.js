e4cf510903f665783bc12762d2bcc62215d80c53
Daryl Jewkes <github@daryljewkes.com>
2014-12-12 12:30:54 +0000

    added config file

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var config = {
+    api: 'http://ergast.com/api/',
+    defaultResponseRows: 200,
+    maxResponseRows: 1000
+};
+
+module.exports = config;
--- js/services.js
+++ js/services.js
@@ -1,31 +1,33 @@
+var config = require('../config');
+
 angular.module('formulaOneApp.services', ['ngResource'])
   .factory('Driver', function($resource) {
     return {
-      standings: $resource('http://ergast.com/api/:series/:season/driverStandings.json', {
+      standings: $resource(config.api + ':series/:season/driverStandings.json', {
         'get': { method:'GET', cache: true, isArray:true }
       }),
-      driver: $resource('http://ergast.com/api/:series/drivers/:id.json', {
+      driver: $resource(config.api + ':series/drivers/:id.json', {
         'get': { method:'GET', cache: true, isArray:true }
       }),
-      driverStandings: $resource('http://ergast.com/api/:series/drivers/:id/driverStandings.json', {
+      driverStandings: $resource(config.api + ':series/drivers/:id/driverStandings.json', {
         'get': { method:'GET', cache: true, isArray:true }
       })
     };
   }).factory('Circuit', function($resource) {
     return {
-      circuits: $resource('http://ergast.com/api/:series/:season/circuits.json', {
+      circuits: $resource(config.api + ':series/:season/circuits.json', {
         'get': { method:'GET', cache: true, isArray:true }
       }),
-      circuit: $resource('http://ergast.com/api/:series/circuits/:id.json', {
+      circuit: $resource(config.api + ':series/circuits/:id.json', {
         'get': { method:'GET', cache: true, isArray:true }
       })
     };
   }).factory('Constructor', function($resource) {
     return {
-      standings: $resource('http://ergast.com/api/:series/:season/constructorStandings.json', {
+      standings: $resource(config.api + ':series/:season/constructorStandings.json', {
         'get': { method:'GET', cache: true, isArray:true }
       }),
-      constructor: $resource('http://ergast.com/api/:series/constructors/:id/constructorStandings.json', {
+      constructor: $resource(config.api + ':series/constructors/:id/constructorStandings.json', {
         'get': { method:'GET', cache: true, isArray:true }
       })
     }
