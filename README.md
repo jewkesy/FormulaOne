03d2e1ae01287ee285a8f87f8ca53a5fcd7924db
Daryl Jewkes <github@daryljewkes.com>
2014-12-18 20:54:29 +0000

    starting to add some server side logic to grab iamges for local use

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Web application rendering stats from Formula 1
 http://jewkesy.github.io/FormulaOne/
 
 Uses http://ergast.com/mrd/ as a data source
+
+Utils.js is a NodeJS application to download imagery from Wikipedia for deployment
---
+++ utils.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+var request = require('request');
+
+var wikiApi = "http://en.wikipedia.org/w/api.php?format=json&action=query&redirects&prop=pageimages&titles=",
+
+download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream('./images/drivers/' + filename)).on('close', callback);
+  });
+};
+
+downloadWikiPic = function(url, saveFileName, callback){
+  request({ url: url, json: true }, function(err, res, body){
+
+    for (var page in body.query.pages) {
+      for (var thumbnail in body.query.pages[page].thumbnail) {
+        for (var source in body.query.pages[page].thumbnail[thumbnail]) {
+          console.log(body.query.pages[page].thumbnail[thumbnail[source]])
+        }
+      }
+    }
+
+    //request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    // download(url, filename, function(){
+    //   console.log('done');
+    // });
+  });
+};
+
+
+
+function processDriverPics(url) { request({ url: url, json: true }, function (error, response, body) {
+  if (error) { return console.log(error)};
+  if (response.statusCode === 200) {
+    var drivers = body.MRData.DriverTable.Drivers;
+    for (var driver in drivers) {
+      //console.log(drivers[driver].givenName)
+      var wikiUrl = drivers[driver].url.split("/");
+      wikiUrl = wikiUrl[wikiUrl.length - 1];
+
+      downloadWikiPic(wikiApi + wikiUrl + "&pithumbsize=" + 200, drivers[driver].driverId, function() {console.log('done ' + wikiUrl)})
+    }
+  }
+})
+}
+
+// driver profile pics
+
+// get list of drivers
+// http://ergast.com/api/f1/drivers.json?limit=1000
+processDriverPics("http://ergast.com/api/f1/drivers.json?limit=1000")
+
+// circuits
+
+// constructors
