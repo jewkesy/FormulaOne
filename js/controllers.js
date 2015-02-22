2e9f6eef59d4b4017a535b1335569ec02fc71781
Daryl Jewkes <github@daryljewkes.com>
2015-02-22 20:01:37 +0000

    starting ot add mongodb data;

--- js/controllers.js
+++ js/controllers.js
@@ -140,6 +140,7 @@ angular.module('formulaOneApp.controllers', [])
 
 }).controller('ConstructorViewController', function($scope, $rootScope, $http, $timeout, $stateParams, Constructor) {
   $scope.data = Constructor.constructor.get({ id: $stateParams.id, series: 'f1' }, function(){
+console.log($scope)    
     $scope.content_loaded = true;
     var retVal = $scope.data.MRData.StandingsTable;
     $rootScope.title = " .:. FormulaOne Stats .:. Constructors .:. " + retVal.StandingsLists[0].ConstructorStandings[0].Constructor.name;
