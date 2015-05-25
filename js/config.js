abe65b7ebf5362c2441554db96a69b5f1c799619
Daryl Jewkes <github@daryljewkes.com>
2015-05-25 21:48:38 +0100

    new logo in header; tweak to news feed for more results

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ var config = {
     maxResponseRows: 1000,
     flagsUrl: "http://jewkesy.github.io/colloquial/images/flags/",
     wikiApi: "http://en.wikipedia.org/w/api.php?callback=JSON_CALLBACK&format=json&action=query&redirects&prop=pageimages&titles=",
-    googleNews: "http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=f1%20news&callback=JSON_CALLBACK",
+    googleNews: "http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=f1&callback=JSON_CALLBACK",
     picNarrowSize: 320,
     picThumbSize: 50,
     picWideSize: 640,
