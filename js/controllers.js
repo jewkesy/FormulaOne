b4fcfd0b7621de15ff8b8486f61b82d8d15797d4
Daryl Jewkes <github@daryljewkes.com>
2015-11-01 21:48:18 +0000

    dropped wiki linkks from circuits view

--- js/controllers.js
+++ js/controllers.js
@@ -411,7 +411,7 @@ angular.module('formulaOneApp.controllers', ['ngSanitize'])
   }
 
   function buildConstructor(constructor) {
-    console.log(constructor)
+    // console.log(constructor)
     var retVal = constructor.MRData.StandingsTable;
     if (retVal.StandingsLists.length == 0) { $scope.content_loaded=true; $scope.no_data=true;$scope.chartLoaded = true; return false;}
     $rootScope.title = "Formula One Stats .:. Constructors .:. " + retVal.StandingsLists[0].ConstructorStandings[0].Constructor.name;
