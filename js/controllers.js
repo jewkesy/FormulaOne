b7c119fb89d41230c67ac4c0f4c15c2af9d4baf0
Daryl Jewkes <github@daryljewkes.com>
2015-11-01 10:55:54 +0000

    added graphics to constructors standings.  Now shows points, rather than wins

--- js/controllers.js
+++ js/controllers.js
@@ -76,11 +76,11 @@ angular.module('formulaOneApp.controllers', ['ngSanitize'])
 
     for (x = 0; x < retVal.DriverStandings.length; x++) {
       var win = retVal.DriverStandings[x]
-
       if (typeof win.Driver.code == 'undefined') win.Driver.code = win.Driver.familyName
       winDetails.chartLabels.push(win.Driver.code)
       winDetails.chartSeries.push(win.Driver.code)
-      winDetails.chartData[0].push(win.wins)
+      winDetails.chartData[0].push(win.points)
+      // winDetails.chartData[0].push(win.wins)
     }
 
     buildDriversChart(winDetails)
@@ -287,7 +287,47 @@ angular.module('formulaOneApp.controllers', ['ngSanitize'])
     var retVal = $scope.data.MRData.StandingsTable.StandingsLists[0]
     //console.log(retVal)
     $scope.teams = retVal //NOTE constructor is a reserved AngularJs name!
+
+    var winDetails = {
+      chartData: [[]],
+      chartSeries: [],
+      chartLabels: []
+    }
+// console.log(retVal)
+    for (x = 0; x < retVal.ConstructorStandings.length; x++) {
+      var win = retVal.ConstructorStandings[x]
+
+      winDetails.chartLabels.push(win.Constructor.name)
+      winDetails.chartSeries.push(win.Constructor.name)
+      winDetails.chartData[0].push(win.points)
+      // winDetails.chartData[0].push(win.wins)
+    }
+
+    buildConstructorsChart(winDetails)
   });
+
+  function buildConstructorsChart(winDetails) {
+    // console.log(winDetails.chartLabels, winDetails.chartData, winDetails.chartSeries)
+    $scope.chartLabels = winDetails.chartLabels;
+    $scope.chartData = winDetails.chartData;
+    // $scope.series = winDetails.chartSeries;
+
+    var width = $window.innerWidth
+    if (width <= 400) 
+      $scope.chartOptions = {legend: true, animation: false}
+    else if (width <= 640)
+      $scope.chartOptions = {legend: true, animation: true, animationStep: 10}
+    else 
+      $scope.chartOptions = {legend: true, animation: true}
+    
+    $scope.onClick = function (points, evt) {
+      // console.log(points, evt);
+    };
+    $scope.$on('create', function () {
+       $scope.chartLoaded = true;
+    });
+  }
+  
 
   $scope.$watch("season", function( value ) {
       if (value >= 1950) {
